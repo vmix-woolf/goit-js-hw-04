1b4befdaa7fc24dfec32b05d8fd88004b355799e
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-04-07 16:30:18 +0300

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    }
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
